5508087227cbd210ccc0ba4810b23d95af168af0
Denis Ceric <denisceric94@gmail.com>
2019-10-01 00:55:53 +0200

    Landing page design and paypal functionality

---
+++ resources/js/components/Subscribe.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="container d-flex justify-content-center">
+        <div class="jumbotron bg-dark2 shadow-sm my-auto text-center w-50">
+            <div class="pb-5">Enter your e-mail and get lifetime of great daily quotes!</div>
+            <input class="form-control form-control-lg w-100" type="text" placeholder="email@example.com">
+            <div class="w-75 mx-auto">
+                <PayPal
+                    class="mt-5"
+                    amount="10.00"
+                    currency="USD"
+                    :client="paypal"
+                    env="sandbox"
+                    :button-style="btnStyle"
+                    @payment-completed="paymentCompleted"
+                    @payment-cancelled="paymentCancelled">
+                </PayPal>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import PayPal from 'vue-paypal-checkout'
+    export default {
+        name: 'subscribe',
+        components: { 
+            PayPal
+        },
+        data() {
+            return {
+                paypal: {
+                    sandbox: 'ASl2s1url0eCesOPuiS6Va93vCRnYk_-fmWMH5FHde9WJQetaaAe9uZJkJmQOJ4x6F2NsZkP2o8IHTzJ'
+                },
+                btnStyle: {
+                    label: 'buynow',
+                    size:  'responsive',
+                    shape: 'rect',
+                    color: 'blue'
+                }
+            }
+        },
+        methods: {
+            paymentCompleted() {
+                console.log('Completed!');
+            },
+            paymentCancelled() {
+                console.log('Cancelled!');
+            }
+        }
+    }
+</script>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,91 +10,27 @@
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
 
         <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+        <link href="{{ asset('css/custom.css') }}" rel="stylesheet">
 
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
     </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
+    <body class="h-100">
+        <div id="app">
+        <div class="container h-25 d-flex justify-content-center p-5">
+            <div class="my-auto text-center">
+                <h2 class="display-4">DAILY QUOTES</h2>
+                <hr>
+                <blockquote class="blockquote w-75 mx-auto bg-light text-dark text-left">
+                    <p class="mb-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                    <footer class="blockquote-footer text-right text-dark"><cite title="Source Title">Cicero</cite></footer>
+                </blockquote>
             </div>
         </div>
+        <subscribe></subscribe>
+        </div>
+
+
+        <!-- Scripts -->
+        <script src="{{ asset('js/app.js') }}" defer></script>
     </body>
 </html>
