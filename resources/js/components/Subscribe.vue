1c5a435596339d47f25ead190c344de45b678ae7
Denis Ceric <denisceric94@gmail.com>
2019-10-01 12:21:14 +0200

    Added client side email validation

--- resources/js/components/Subscribe.vue
+++ resources/js/components/Subscribe.vue
@@ -1,9 +1,16 @@
 <template>
     <div class="container d-flex justify-content-center">
         <div class="jumbotron bg-dark2 shadow-sm my-auto text-center w-50">
-            <div class="pb-5">Enter your e-mail and get lifetime of great daily quotes!</div>
-            <input class="form-control form-control-lg w-100" type="text" placeholder="email@example.com">
-            <div class="w-75 mx-auto">
+            <div class="pb-5">Enter your e-mail and get a lifetime of great daily quotes!</div>
+
+            <div class="form-group">
+            <input type="text" :class="['form-control', isEmailValid()]" id="inputSuccess1" v-model="email">
+            </div>
+
+            <div class="w-75 mx-auto" v-if="isEmailValid() !== 'is-valid'">
+                <div class="paypal-button paypal-button-color mt-5"> <span class="paypal-button-text"> Buy Now</span></div>
+            </div>
+            <div class="w-75 mx-auto" v-else>
                 <PayPal
                     class="mt-5"
                     amount="10.00"
@@ -15,6 +22,7 @@
                     @payment-cancelled="paymentCancelled">
                 </PayPal>
             </div>
+
         </div>
     </div>
 </template>
@@ -36,10 +44,16 @@
                     size:  'responsive',
                     shape: 'rect',
                     color: 'blue'
-                }
+                },
+                email: '',
+                isValidated: false,
+                reg: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,24}))$/
             }
         },
         methods: {
+            isEmailValid() {
+                return (this.email == "")? "" : (this.reg.test(this.email)) ? 'is-valid' : 'is-invalid';
+            },
             paymentCompleted() {
                 console.log('Completed!');
             },
@@ -49,3 +63,39 @@
         }
     }
 </script>
+
+<style scoped>
+
+.paypal-button {
+    font-family: Arial, Helvetica, sans-serif;
+    font-size: 20px;
+    font-weight:900;
+    height: 48px;
+    border-radius: 4px;
+    color: white;
+    cursor: no-drop;
+}
+
+.paypal-button-text {
+    display: inline-block;
+    white-space: pre-wrap;
+}
+
+.paypal-button .paypal-button-text {
+    vertical-align: top;
+    position: relative;
+    top: 50%;
+    transform: translateY(-50%);
+    -webkit-transform: translateY(-50%);
+    -moz-transform: translateY(-50%);
+    -ms-transform: translateY(-50%);
+    -o-transform: translateY(-50%);
+    text-align: left;
+}
+
+.paypal-button-color {
+    background-color: silver;
+}
+
+
+</style>
